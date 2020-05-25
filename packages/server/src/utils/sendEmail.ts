bf8c3574eb4c45d7962a7589aee98806153f16a2
btvinh11 <vinh.tdm.hn@gmail.com>
2020-05-25 15:26:14 +0700

    nodemailer

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,22 +1,40 @@
-import * as SparkPost from "sparkpost";
-const client = new SparkPost(process.env.SPARKPOST_API_KEY);
+import * as nodemailer from "nodemailer";
 
 export const sendEmail = async (recipient: string, url: string) => {
-  const response = await client.transmissions.send({
-    options: {
-      sandbox: true,
-    },
-    content: {
-      from: "testing@sparkpostbox.com",
-      subject: "Confirm Email",
+  try {
+    const testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: `Recipient <${recipient}>`, // list of receivers
+      subject: "Confirm Email", // Subject line
+      text: "Hello,", // plain text body
       html: `<html>
-            <body>
-            <p>Testing SparkPost - the world's most awesomest email service!</p>
-            <a href="${url}">confirm email</a>
-            </body>
-        </html>`,
-    },
-    recipients: [{ address: recipient }],
-  });
-  console.log(response);
+          <body>
+          <a href="${url}">Please confirm email</a>
+          </body>
+      </html>`,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
